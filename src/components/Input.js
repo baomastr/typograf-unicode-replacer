f1be8d1903324bb18df8d5ef9a20ea0ca1425509
Dmitriy Lisyanskiy <d.lisyanskiy@tinkoff.ru>
2018-06-27 11:49:26 +0300

    added ndash

--- src/components/Input.js
+++ src/components/Input.js
@@ -24,6 +24,7 @@ export default class Input extends PureComponent {
     const replaced = this.state.value
       .replace(/&nbsp;/g, `\\u00a0`)
       .replace(/&mdash;/g, `\\u2014`)
+      .replace(/&ndash;/g, `\\u2013`)
       .replace(/&laquo;/g, `\\u00AB`)
       .replace(/&raquo;/g, `\\u00BB`);
     this.setState({ output: replaced });
