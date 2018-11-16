98e3a56a4896ddf73758f0598c62c45ffba8fb07
baomastr <dmytro.lysyansky@gmail.com>
2018-11-16 14:40:23 +0300

    disable number/fraction and groupNumbers rules, add new groupNumbrs rule

--- src/utils/typograf.js
+++ src/utils/typograf.js
@@ -1,12 +1,24 @@
 // https://github.com/typograf/typograf
 import Typografer from "typograf";
 
-Typografer.addRule({
-    name: 'common/other/ALLGames',
-    handler: function (text) {
-        return text.replace(/ALL Games/ig, 'ALL\u00A0Games');
+Typografer
+    .addRule({
+    name: 'ru/nbsp/groupNumbersNew',
+    handler(text) {
+        const re = new RegExp(`(^ ?|\\D |${Typografer._privateLabel})(\\d{1,3}([ \u00A0\u202F\u2009]\\d{3})+)(?! ?[\\d-])`, 'gm');
+
+        return text.replace(re, function($0, $1, $2) {
+            return $1 + $2.replace(/\s/g, '\u00A0');
+        });
     }
-});
+    })
+
+    .addRule({
+        name: 'common/other/ALLGames',
+        handler: function (text) {
+            return text.replace(/ALL Games/ig, 'ALL\u00A0Games');
+        }
+    });
 
 const typograf = new Typografer({
     locale: ["ru", "en-US"],
@@ -22,7 +34,9 @@ typograf
     // .enableRule('common/other/ALLGames')
     .enableRule('common/punctuation/*')
     .enableRule('common/nbsp/afterNumber')
-    .enableRule('common/symbols/*');
+    .enableRule('common/symbols/*')
+    .disableRule('ru/nbsp/groupNumbers')
+    .disableRule('common/number/fraction');
 
 export const typografer = input => {
     return typograf.execute(input);
