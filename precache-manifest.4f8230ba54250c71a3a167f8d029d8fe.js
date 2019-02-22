6449f8bbaad9540fdd38bc6cbeee144d4a8db66c
macuser <macuser@mbpro.local>
2019-02-22 12:43:40 +0300

    Updates

---
+++ precache-manifest.4f8230ba54250c71a3a167f8d029d8fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef878bb96dec46706df3",
+    "url": "/typograf-unicode-replacer/static/js/runtime~main.ef878bb9.js"
+  },
+  {
+    "revision": "1713db1b08311dc57159",
+    "url": "/typograf-unicode-replacer/static/js/main.1713db1b.chunk.js"
+  },
+  {
+    "revision": "4e6cfac690997725365e",
+    "url": "/typograf-unicode-replacer/static/js/2.4e6cfac6.chunk.js"
+  },
+  {
+    "revision": "1713db1b08311dc57159",
+    "url": "/typograf-unicode-replacer/static/css/main.ff23651e.chunk.css"
+  },
+  {
+    "revision": "364a5f0abb272fdf96ed63a3b5b255c7",
+    "url": "/typograf-unicode-replacer/index.html"
+  }
+];
