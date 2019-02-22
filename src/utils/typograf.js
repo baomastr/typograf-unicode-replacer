e80e509ebff41ecc7bff21e345af508123566532
macuser <macuser@mbpro.local>
2019-02-22 12:40:45 +0300

    update packages and rules for typograf

--- src/utils/typograf.js
+++ src/utils/typograf.js
@@ -2,55 +2,63 @@
 import Typografer from "typograf";
 
 Typografer
-    .addRule({
-        name: 'ru/nbsp/groupNumbersNew',
-        handler(text) {
-            const re = new RegExp(`(^ ?|\\D |${Typografer._privateLabel})(\\d{1,3}([ \u00A0\u202F\u2009]\\d{3})+)(?! ?[\\d-])`, 'gm');
+  .addRule({
+    name: 'ru/nbsp/groupNumbersNew',
+    handler(text) {
+      const re = new RegExp(`(^ ?|\\D |${Typografer._privateLabel})(\\d{1,3}([ \u00A0\u202F\u2009]\\d{3})+)(?! ?[\\d-])`, 'gm');
 
-            return text.replace(re, function($0, $1, $2) {
-                return $1 + $2.replace(/\s/g, '\u00A0');
-            });
-        }
-    })
+      return text.replace(re, function ($0, $1, $2) {
+        return $1 + $2.replace(/\s/g, '\u00A0');
+      });
+    }
+  })
 
-    .addRule({
-        name: 'common/nbsp/TinkoffBank',
-        handler: function (text) {
-            return text.replace(/(Тинькофф)\s+(Банк)/ig, '$1\u00A0$2');
-        }
-    })
+  .addRule({
+    name: 'common/nbsp/TinkoffBank',
+    handler: function (text) {
+      return text.replace(/(Тинькофф)\s+(Банк)/ig, '$1\u00A0$2');
+    }
+  })
 
-    .addRule({
-        name: 'common/nbsp/betweenWordAndNumber',
-        handler: function (text) {
-            return text.replace(/([\wа-я]+) (\d+)/ig, '$1\u00A0$2');
-        }
-    })
+  .addRule({
+    name: 'common/nbsp/TinkoffBlack',
+    handler: function (text) {
+      return text.replace(/(Tinkoff)\s+(Black)/ig, '$1\u00A0$2');
+    }
+  })
 
-    .addRule({
-        name: 'common/nbsp/ALLGames',
-        handler: function (text) {
-            return text.replace(/ALL Games/ig, 'ALL\u00A0Games');
-        }
-    });
+  .addRule({
+    name: 'common/nbsp/betweenWordAndNumber',
+    handler: function (text) {
+      return text.replace(/([\wа-я]+) (\d+)/ig, '$1\u00A0$2');
+    }
+  })
+
+  .addRule({
+    name: 'common/nbsp/ALLGames',
+    handler: function (text) {
+      return text.replace(/ALL Games/ig, 'ALL\u00A0Games');
+    }
+  });
 
 const typograf = new Typografer({
-    locale: ["ru", "en-US"],
-    live: true,
-    htmlEntity: {
-        type: "name",
-        onlyInvisible: true
-    }
+  locale: ["ru", "en-US"],
+  live: true,
+  htmlEntity: {
+    type: "name",
+    onlyInvisible: true
+  }
 });
 
 typograf
-    .enableRule('ru/money/currency')
-    .enableRule('common/punctuation/*')
-    .enableRule('common/nbsp/afterNumber')
-    .enableRule('common/symbols/*')
-    .disableRule('ru/nbsp/groupNumbers')
-    .disableRule('common/number/fraction');
+  .enableRule('ru/money/currency')
+  .enableRule('common/punctuation/*')
+  .enableRule('common/nbsp/afterNumber')
+  .enableRule('common/symbols/*')
+  .disableRule('ru/nbsp/groupNumbers')
+  .disableRule('ru/date/fromISO')
+  .disableRule('common/number/fraction');
 
 export const typografer = input => {
-    return typograf.execute(input);
+  return typograf.execute(input);
 };
