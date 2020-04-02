dde3a4176be6f7aef59e2fc04993a212e344d92c
d.lisyanskiy <d.lisyanskiy@tinkoff.ru>
2020-04-02 11:56:37 +0300

    update patch function

--- src/utils/patch.js
+++ src/utils/patch.js
@@ -1,20 +1,23 @@
 import {typografer} from './typograf';
 
-export const patch = root => {
+export const patch = (root, transform = typografer, parentKey = '') => {
   if (Array.isArray(root)) {
-    return root.map(el => patch(el));
+    return root.map(el => patch(el, transform));
   } else if (root !== null && typeof root === 'object') {
     return Object.entries(root).reduce((acc, [k, v]) => {
-      if (typeof v === 'string') {
-        acc[k] = typografer(v);
-      } else {
-        acc[k] = patch(v);
-      }
+      // if parent key starts with underscore use it. else use key got in current iteration. for recursive ignoring
+      const newParentKey = parentKey && parentKey[0] === '_' ? parentKey : k;
+      const isIgnoreKey = newParentKey[0] === '_';
 
+      if (typeof v === 'string') {
+        acc[k] = isIgnoreKey ? v : transform(v);
+      } else {
+        acc[k] = patch(v, isIgnoreKey ? str => str : transform, newParentKey);
+      }
       return acc;
     }, {});
   } else if (typeof root === 'string') {
-    return typografer(root);
+    return transform(root);
   } else {
     return root;
   }
